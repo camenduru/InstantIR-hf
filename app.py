025b1f9910451fded0cf3bd34f086199cd454261
DannHuang <jenyuan@stu.pku.edu.cn>
2024-11-04 09:11:10 +0800

    remove local diffusers

--- app.py
+++ app.py
@@ -1,45 +1,26 @@
 import os
 import torch
-import random
 import numpy as np
-import gradio as gr
+import app as gr
 from PIL import Image
-from torchvision import transforms
 
-from diffusers import (
-    DDPMScheduler,
-    StableDiffusionXLPipeline
-)
+from diffusers import DDPMScheduler
 from schedulers.lcm_single_step_scheduler import LCMSingleStepScheduler
-from diffusers.utils import convert_unet_state_dict_to_peft
-from peft import LoraConfig, set_peft_model_state_dict
-from transformers import (
-    AutoImageProcessor, AutoModel
-)
 
-from module.ip_adapter.utils import init_ip_adapter_in_unet
-from module.ip_adapter.resampler import Resampler
-from module.aggregator import Aggregator
-from pipelines.sdxl_instantir import InstantIRPipeline, LCM_LORA_MODULES, PREVIEWER_LORA_MODULES
+from module.ip_adapter.utils import load_adapter_to_pipe
+from pipelines.sdxl_instantir import InstantIRPipeline
 
 from huggingface_hub import hf_hub_download
 
-if not os.path.exists("checkpoints/adapter.pt"):
-    hf_hub_download(repo_id="InstantX/InstantIR", filename="adapter.pt", local_dir="./checkpoints")
-if not os.path.exists("checkpoints/aggregator.pt"):
-    hf_hub_download(repo_id="InstantX/InstantIR", filename="aggregator.pt", local_dir="./checkpoints")
-if not os.path.exists("checkpoints/previewer_lora_weights.bin"):
-    hf_hub_download(repo_id="InstantX/InstantIR", filename="previewer_lora_weights.bin", local_dir="./checkpoints")
-
-
-transform = transforms.Compose([
-    transforms.Resize(1024, interpolation=transforms.InterpolationMode.BILINEAR),
-    transforms.CenterCrop(1024),
-])
+if not os.path.exists("models/adapter.pt"):
+    hf_hub_download(repo_id="InstantX/InstantIR", filename="models/adapter.pt", local_dir=".")
+if not os.path.exists("models/aggregator.pt"):
+    hf_hub_download(repo_id="InstantX/InstantIR", filename="models/aggregator.pt", local_dir=".")
+if not os.path.exists("models/previewer_lora_weights.bin"):
+    hf_hub_download(repo_id="InstantX/InstantIR", filename="models/previewer_lora_weights.bin", local_dir=".")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 sdxl_repo_id = "stabilityai/stable-diffusion-xl-base-1.0"
-instantir_repo_id = "InstantX/InstantIR"
 dinov2_repo_id = "facebook/dinov2-large"
 
 if torch.cuda.is_available():
@@ -47,105 +28,30 @@ if torch.cuda.is_available():
 else:
     torch_dtype = torch.float32
 
-print("Loading vision encoder...")
-image_encoder = AutoModel.from_pretrained(dinov2_repo_id, torch_dtype=torch_dtype)
-image_processor = AutoImageProcessor.from_pretrained(dinov2_repo_id)
-
+# Load pretrained models.
 print("Loading SDXL...")
-pipe = StableDiffusionXLPipeline.from_pretrained(
+pipe = InstantIRPipeline.from_pretrained(
     sdxl_repo_id,
-    torch_dtype=torch.float16,
+    torch_dtype=torch_dtype,
 )
-unet = pipe.unet
 
-print("Initializing Aggregator...")
-aggregator = Aggregator.from_unet(unet, load_weights_from_unet=False)
-
+# Image prompt projector.
 print("Loading LQ-Adapter...")
-image_proj_model = Resampler(
-    dim=1280,
-    depth=4,
-    dim_head=64,
-    heads=20,
-    num_queries=64,
-    embedding_dim=image_encoder.config.hidden_size,
-    output_dim=unet.config.cross_attention_dim,
-    ff_mult=4
-)
-init_ip_adapter_in_unet(
-    unet,
-    image_proj_model,
-    "checkpoints/adapter.pt",
-    adapter_tokens=64,
-)
-print("Initializing InstantIR...")
-pipe = InstantIRPipeline(
-        pipe.vae, pipe.text_encoder, pipe.text_encoder_2, pipe.tokenizer, pipe.tokenizer_2,
-        unet, aggregator, pipe.scheduler, feature_extractor=image_processor, image_encoder=image_encoder,
+load_adapter_to_pipe(
+    pipe,
+    "models/adapter.pt",
+    dinov2_repo_id,
 )
 
-# Add Previewer LoRA.
-lora_state_dict, alpha_dict = StableDiffusionXLPipeline.lora_state_dict(
-    "checkpoints/previewer_lora_weights.bin",
-    # weight_name="previewer_lora_weights.bin",
-
-)
-unet_state_dict = {
-    f'{k.replace("unet.", "")}': v for k, v in lora_state_dict.items() if k.startswith("unet.")
-}
-unet_state_dict = convert_unet_state_dict_to_peft(unet_state_dict)
-lora_state_dict = dict()
-for k, v in unet_state_dict.items():
-    if "ip" in k:
-        k = k.replace("attn2", "attn2.processor")
-        lora_state_dict[k] = v
-    else:
-        lora_state_dict[k] = v
-if alpha_dict:
-    lora_alpha = next(iter(alpha_dict.values()))
-else:
-    lora_alpha = 1
+# Prepare previewer
+lora_alpha = pipe.prepare_previewers("models")
 print(f"use lora alpha {lora_alpha}")
-lora_config = LoraConfig(
-    r=64,
-    target_modules=PREVIEWER_LORA_MODULES,
-    lora_alpha=lora_alpha,
-    lora_dropout=0.0,
-)
-
-# Add LCM LoRA.
-lora_state_dict, alpha_dict = StableDiffusionXLPipeline.lora_state_dict(
-    "latent-consistency/lcm-lora-sdxl"
-)
-unet_state_dict = {
-    f'{k.replace("unet.", "")}': v for k, v in lora_state_dict.items() if k.startswith("unet.")
-}
-unet_state_dict = convert_unet_state_dict_to_peft(unet_state_dict)
-if alpha_dict:
-    lora_alpha = next(iter(alpha_dict.values()))
-else:
-    lora_alpha = 1
+lora_alpha = pipe.prepare_previewers("latent-consistency/lcm-lora-sdxl", use_lcm=True)
 print(f"use lora alpha {lora_alpha}")
-lora_config = LoraConfig(
-    r=64,
-    target_modules=LCM_LORA_MODULES,
-    lora_alpha=lora_alpha,
-    lora_dropout=0.0,
-)
+pipe.to(device=device, dtype=torch_dtype)
+pipe.scheduler = DDPMScheduler.from_pretrained(sdxl_repo_id, subfolder="scheduler")
+lcm_scheduler = LCMSingleStepScheduler.from_config(pipe.scheduler.config)
 
-unet.add_adapter(lora_config, "lcm")
-incompatible_keys = set_peft_model_state_dict(unet, unet_state_dict, adapter_name="lcm")
-if incompatible_keys is not None:
-    # check only for unexpected keys
-    unexpected_keys = getattr(incompatible_keys, "unexpected_keys", None)
-    missing_keys = getattr(incompatible_keys, "missing_keys", None)
-    if unexpected_keys:
-        raise ValueError(
-            f"Loading adapter weights from state_dict led to unexpected keys not found in the model: "
-            f" {unexpected_keys}. "
-        )
-
-unet.disable_adapters()
 pipe.scheduler = DDPMScheduler.from_pretrained(
     sdxl_repo_id,
     subfolder="scheduler"
@@ -154,16 +60,24 @@ lcm_scheduler = LCMSingleStepScheduler.from_config(pipe.scheduler.config)
 # Load weights.
 print("Loading checkpoint...")
 aggregator_state_dict = torch.load(
-    "checkpoints/aggregator.pt",
+    "models/aggregator.pt",
     map_location="cpu"
 )
-aggregator.load_state_dict(aggregator_state_dict, strict=True)
-aggregator.to(dtype=torch.float16)
-unet.to(dtype=torch.float16)
-pipe=pipe.to(device)
+pipe.aggregator.load_state_dict(aggregator_state_dict, strict=True)
+pipe.aggregator.to(device=device, dtype=torch_dtype)
 
 MAX_SEED = np.iinfo(np.int32).max
 MAX_IMAGE_SIZE = 1024
+
+PROMPT = "Photorealistic, highly detailed, hyper detailed photo - realistic maximum detail, 32k, \
+ultra HD, extreme meticulous detailing, skin pore detailing, \
+hyper sharpness, perfect without deformations, \
+taken using a Canon EOS R camera, Cinematic, High Contrast, Color Grading. "
+
+NEG_PROMPT = "blurry, out of focus, unclear, depth of field, over-smooth, \
+sketch, oil painting, cartoon, CG Style, 3D render, unreal engine, \
+dirty, messy, worst quality, low quality, frames, painting, illustration, drawing, art, \
+watermark, signature, jpeg artifacts, deformed, lowres"
 
 def unpack_pipe_out(preview_row, index):
     return preview_row[index][0]
@@ -179,37 +93,45 @@ def show_final_preview(preview_row):
     return preview_row[-1][0]
 
 # @spaces.GPU #[uncomment to use ZeroGPU]
-def instantir_restore(lq, prompt="", steps=30, cfg_scale=7.0, guidance_end=1.0, creative_restoration=False, seed=3407):
+@torch.no_grad()
+def instantir_restore(
+    lq, prompt="", steps=30, cfg_scale=7.0, guidance_end=1.0,
+    creative_restoration=False, seed=3407, height=1024, width=1024, preview_start=0.0):
     if creative_restoration:
         if "lcm" not in pipe.unet.active_adapters():
             pipe.unet.set_adapter('lcm')
     else:
-        if "previewer" not in pipe.unet.active_adapters():
-            pipe.unet.set_adapter('previewer')
+        if "default" not in pipe.unet.active_adapters():
+            pipe.unet.set_adapter('default')
 
     if isinstance(guidance_end, int):
         guidance_end = guidance_end / steps
-    with torch.no_grad(): lq = [transform(lq)]
+    if isinstance(preview_start, int):
+        preview_start = preview_start / steps
+    lq = [resize_img(lq.convert("RGB"), size=(width, height))]
     generator = torch.Generator(device=device).manual_seed(seed)
+    timesteps = [
+        i * (1000//steps) + pipe.scheduler.config.steps_offset for i in range(0, steps)
+    ]
+    timesteps = timesteps[::-1]
+    start_timestep = timesteps[0]
+
+    prompt = PROMPT if len(prompt)==0 else prompt
+    neg_prompt = NEG_PROMPT
 
     out = pipe(
         prompt=[prompt]*len(lq),
         image=lq,
-        ip_adapter_image=[lq],
         num_inference_steps=steps,
         generator=generator,
-        controlnet_conditioning_scale=1.0,
-        # negative_original_size=(256,256),
-        # negative_target_size=(1024,1024),
-        negative_prompt=[""]*len(lq),
+        timesteps=timesteps,
+        negative_prompt=[neg_prompt]*len(lq),
         guidance_scale=cfg_scale,
         control_guidance_end=guidance_end,
-        # control_guidance_start=0.5,
+        preview_start=preview_start,
         previewer_scheduler=lcm_scheduler,
         return_dict=False,
         save_preview_row=True,
-        # reference_latent = reference_latents,
-        # output_type='pt'
     )
     for i, preview_img in enumerate(out[1]):
         preview_img.append(f"preview_{i}")
@@ -228,7 +150,7 @@ css="""
 }
 """
 
-with gr.Blocks(css=css) as demo:
+with gr.Blocks() as demo:
     gr.Markdown(
     """
     # InstantIR: Blind Image Restoration with Instant Generative Reference.
@@ -242,41 +164,36 @@ with gr.Blocks(css=css) as demo:
     """)
     with gr.Row():
         lq_img = gr.Image(label="Low-quality image", type="pil")
-        with gr.Column(elem_id="col-container"):
+        with gr.Column():
             with gr.Row():
-                steps = gr.Number(label="Steps", value=20, step=1)
+                steps = gr.Number(label="Steps", value=30, step=1)
                 cfg_scale = gr.Number(label="CFG Scale", value=7.0, step=0.1)
+            with gr.Row():
+                height = gr.Number(label="Height", value=1024, step=1)
+                weight = gr.Number(label="Weight", value=1024, step=1)
                 seed = gr.Number(label="Seed", value=42, step=1)
             # guidance_start = gr.Slider(label="Guidance Start", value=1.0, minimum=0.0, maximum=1.0, step=0.05)
-            guidance_end = gr.Slider(label="Start Free Rendering", value=20, minimum=0, maximum=20, step=1)
-            prompt = gr.Textbox(
-                label="Restoration prompts (Optional)", show_label=False,
-                placeholder="Restoration prompts (Optional)", value='',
-                # container=False,
-            )
+            guidance_end = gr.Slider(label="Start Free Rendering", value=30, minimum=0, maximum=30, step=1)
+            preview_start = gr.Slider(label="Preview Start", value=0, minimum=0, maximum=30, step=1)
+            prompt = gr.Textbox(label="Restoration prompts (Optional)", placeholder="")
             mode = gr.Checkbox(label="Creative Restoration", value=False)
-    # with gr.Accordion("Advanced Settings", open=False):
     with gr.Row():
         with gr.Row():
             restore_btn = gr.Button("InstantIR magic!")
             clear_btn = gr.ClearButton()
-        index = gr.Slider(label="Restoration Previews", value=19, minimum=0, maximum=19, step=1)
+        index = gr.Slider(label="Restoration Previews", value=29, minimum=0, maximum=29, step=1)
     with gr.Row():
         output = gr.Image(label="InstantIR restored", type="pil")
         preview = gr.Image(label="Preview", type="pil")
-    # gr.Examples(
-    #     examples = examples,
-    #     inputs = [prompt]
-    # )
-    # gr.on(
-    #     triggers=[restore_btn.click, prompt.submit],
-    #     fn = infer,
-    #     inputs = [prompt, negative_prompt, seed, randomize_seed, width, height, guidance_scale, num_inference_steps],
-    #     outputs = [result, seed]
-    # )
     pipe_out = gr.Gallery(visible=False)
     clear_btn.add([lq_img, output, preview])
-    restore_btn.click(instantir_restore, inputs=[lq_img, prompt, steps, cfg_scale, guidance_end, mode, seed], outputs=[output, pipe_out], api_name="InstantIR")
+    restore_btn.click(
+        instantir_restore, inputs=[
+            lq_img, prompt, steps, cfg_scale, guidance_end,
+            mode, seed, height, weight, preview_start,
+        ],
+        outputs=[output, pipe_out], api_name="InstantIR"
+    )
     steps.change(dynamic_guidance_slider, inputs=steps, outputs=guidance_end)
     output.change(dynamic_preview_slider, inputs=steps, outputs=index)
     index.release(unpack_pipe_out, inputs=[pipe_out, index], outputs=preview)
@@ -312,4 +229,4 @@ with gr.Blocks(css=css) as demo:
     ```
     """)
 
-demo.queue().launch(debug=True)
+demo.queue().launch()
