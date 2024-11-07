15477e49da8dd38331d470a3253e1e610cd91e55
Jen-Yuan Huang <JOY-Huang@users.noreply.huggingface.co>
2024-11-07 06:39:17 +0000

    update project description

--- app.py
+++ app.py
@@ -200,7 +200,7 @@ with gr.Blocks() as demo:
     gr.Markdown(
     """
     # InstantIR: Blind Image Restoration with Instant Generative Reference.
-    ### **Official 🤗 Gradio demo of [InstantIR](https://github.com/instantX-research/InstantIR)&nbsp;<a href='https://github.com/instantX-research/InstantIR'><img src='https://img.shields.io/github/stars/instantX-research/InstantIR?style=social'></a>**
+    ### **Official 🤗 Gradio demo of [InstantIR](https://github.com/instantX-research/InstantIR)&nbsp;<a href='https://github.com/instantX-research/InstantIR'><img src='https://img.shields.io/github/stars/instantX-research/InstantIR'></a>**
     ### **InstantIR can not only help you restore your broken image, but also capable of imaginative re-creation following your text prompts. See advance usage for more details!**
     ## Basic usage: revitalize your image
     1. Upload an image you want to restore;
